92e6e8aedebec2277e1ebdfa98f92f517f622612
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2025-04-16 19:51:28 -0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const app = require('./config/express')();
 const port = app.get('port');
 
+app.use(cors(
+    {
+        "origin": "*",
+        "methods": "GET,PUT,POST,DELETE",
+    }
+))
+
 app.listen(port, () => {
     console.log(`Servidor está rodando na porta ${port}`);
 });
